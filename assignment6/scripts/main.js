ed6d5c0e4b680815fe40631d11e496ef48a50d0c
Kristin <kristinmyers@outlook.com>
2019-04-27 23:54:51 -0400

    Added js file and an array an object and a while loop

---
+++ assignment6/scripts/main.js
@@ -0,0 +1,27 @@
+//Array
+
+var members = [
+  'Dave Grohl',
+  'Nate Mendel',
+  'Pat Smear',
+  'Taylor Hawkins',
+  'Rami Jaffee',
+  'Chris Shiflett'
+];
+
+//Object
+
+var tour = {
+  Chicago: "July 29 & 30, 2018",
+  Seattle: "September 1, 2018",
+  Edmonton: "September 4, 2018"
+};
+
+//While Loop
+
+var i = 0;
+
+  while (i <5) {
+  console.log('Times running through the loop: '+ i);
+  i++;
+}
